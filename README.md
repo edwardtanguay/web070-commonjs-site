cb642d8bd4f5c86b9aa10eb1d14d53ff6717b5bc
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-10 02:34:32 +0200

    added readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# web070-commonjs-site
+
+## how to use browserify to transpile CommonJS for use in the browser
+
+```
+browserify js/main.js -o js/bundle.js
+```
--- js/bundle.js
+++ js/bundle.js
@@ -5,7 +5,7 @@ const btnInfoElem = document.querySelector('.btnInfo');
 const messageElem = document.querySelector('.message');
 
 btnInfoElem.addEventListener('click', () => {
-	const sentence = tools.sentencize('the button has been clicked');
+	const sentence = tools.sentencize('the button has been clicked (using CommonJS and Browserify)');
 	messageElem.innerText = sentence;
 });
 
