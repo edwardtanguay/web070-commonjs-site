17342083adf44834c46fb81417a20bfd6c66df78
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-10 02:30:48 +0200

    added note

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@ const btnInfoElem = document.querySelector('.btnInfo');
 const messageElem = document.querySelector('.message');
 
 btnInfoElem.addEventListener('click', () => {
-	const sentence = tools.sentencize('the button has been clicked');
+	const sentence = tools.sentencize('the button has been clicked (using CommonJS and Browserify)');
 	messageElem.innerText = sentence;
 });
